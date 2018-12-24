006b604c8939c0d2a0d5410d381a4b929db45243
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2018-12-24 09:26:23 -0600

    Use Spring Boot dependency management; enable JUnit 5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,20 @@
+import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
 import org.gradle.api.JavaVersion.VERSION_11
+import org.springframework.boot.gradle.plugin.SpringBootPlugin
+import org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES
 
 plugins {
     java
     maven
     id("org.springframework.boot") version "2.1.1.RELEASE"
+    id("io.spring.dependency-management") version "1.0.6.RELEASE"
     id("org.flywaydb.flyway") version "5.2.4"
+}
+
+dependencyManagement {
+    imports {
+        mavenBom(BOM_COORDINATES)
+    }
 }
 
 repositories {
@@ -13,21 +23,19 @@ repositories {
 }
 
 dependencies {
-    annotationProcessor("org.projectlombok:lombok:1.18.4")
-    compileOnly("org.projectlombok:lombok:1.18.4")
+    annotationProcessor("org.projectlombok:lombok")
+    compileOnly("org.projectlombok:lombok")
     compile("com.github.spotbugs:spotbugs-annotations:3.1.9")
-    compile("org.flywaydb:flyway-core:5.2.3")
-    compile("org.springframework.boot:spring-boot-starter-actuator:2.1.1.RELEASE")
-    compile("org.springframework.boot:spring-boot-starter-data-jdbc:2.1.1.RELEASE")
-    compile("org.springframework.boot:spring-boot-starter-data-rest:2.1.1.RELEASE")
-    compile("org.springframework.data:spring-data-rest-hal-browser:3.1.3.RELEASE")
-    runtime("com.zaxxer:HikariCP:3.2.0")
-    runtime("org.hsqldb:hsqldb:2.4.1")
-    testCompile("org.assertj:assertj-core:3.11.1")
-    testCompile("org.hsqldb:sqltool:2.4.1")
-    testCompile("org.junit.jupiter:junit-jupiter-engine:5.3.1")
-    testCompile("org.junit.platform:junit-platform-runner:1.3.1")
-    testCompile("org.springframework.boot:spring-boot-starter-test:2.1.1.RELEASE")
+    compile("org.flywaydb:flyway-core")
+    compile("org.springframework.boot:spring-boot-starter-actuator")
+    compile("org.springframework.boot:spring-boot-starter-data-jdbc")
+    compile("org.springframework.boot:spring-boot-starter-data-rest")
+    compile("org.springframework.data:spring-data-rest-hal-browser")
+    runtime("com.zaxxer:HikariCP")
+    runtime("org.hsqldb:hsqldb")
+    testCompile("org.assertj:assertj-core")
+    testCompile("org.junit.jupiter:junit-jupiter-engine")
+    testCompile("org.springframework.boot:spring-boot-starter-test")
 }
 
 group = "hm.binkley.labs"
@@ -43,8 +51,12 @@ tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
 }
 
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
 flyway {
-    url = "jdbc:hsqldb:file:$buildDir/overrides;create=true;shutdown=true"
+    url = "jdbc:hsqldb:hsql://localhost/overrides"
     user = "sa"
 }
 
