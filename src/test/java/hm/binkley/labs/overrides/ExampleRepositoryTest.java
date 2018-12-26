4eb38aae7dfbc12b51d3f3a383724e91c48391e1
Brian Oxley <boxley@thoughtworks.com>
2018-12-26 07:12:38 -0600

    Unique overrides

--- src/test/java/hm/binkley/labs/overrides/ExampleRepositoryTest.java
+++ src/test/java/hm/binkley/labs/overrides/ExampleRepositoryTest.java
@@ -3,11 +3,15 @@ package hm.binkley.labs.overrides;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.test.context.ContextConfiguration;
 
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 @ContextConfiguration(classes = ExampleConfiguration.class)
 @JdbcTest
@@ -25,29 +29,37 @@ public class ExampleRepositoryTest {
 
         assertThat(found).isEmpty();
 
-        final ExampleWrite bob
-                = writeRepository.save(new ExampleWrite(null, "Bob"));
-        final ExampleWrite nancy
-                = writeRepository.save(new ExampleWrite(null, "Nancy"));
-        final ExampleOverrideWrite fred
-                = overrideRepository.save(bob.withOverride("Fred"));
+        final ExampleWrite bob = writeRepository
+                .save(new ExampleWrite(null, "Bob"));
+        final ExampleWrite nancy = writeRepository
+                .save(new ExampleWrite(null, "Nancy"));
+        final ExampleOverrideWrite fred = overrideRepository
+                .save(bob.withOverride("Fred"));
 
-        assertThat(repository.findById(bob.getId()))
-                .isEqualTo(Optional.of(Example.builder()
-                        .id(bob.getId())
-                        .name(fred.getName())
-                        .exampleId(bob.getId())
-                        .build()));
-        assertThat(repository.findById(nancy.getId()))
-                .isEqualTo(Optional.of(Example.builder()
-                        .id(nancy.getId())
-                        .name(nancy.getName())
-                        .exampleId(null)
-                        .build()));
-        assertThat(writeRepository.findByName(bob.getName()))
-                .isEqualTo(Optional.of(ExampleWrite.builder()
-                        .id(bob.getId())
-                        .name(bob.getName())
-                        .build()));
+        assertThat(repository.findById(bob.getId())).isEqualTo(Optional.of(
+                Example.builder().id(bob.getId()).name(fred.getName())
+                        .exampleId(bob.getId()).build()));
+        assertThat(repository.findById(nancy.getId())).isEqualTo(Optional.of(
+                Example.builder().id(nancy.getId()).name(nancy.getName())
+                        .exampleId(null).build()));
+        assertThat(writeRepository.findByName(bob.getName())).isEqualTo(
+                Optional.of(ExampleWrite.builder().id(bob.getId())
+                        .name(bob.getName()).build()));
+    }
+
+    @Test
+    public void shouldPreventDuplicateOverrides() {
+        final ExampleWrite bob = writeRepository
+                .save(new ExampleWrite(null, "Bob"));
+        overrideRepository
+                .save(new ExampleOverrideWrite(null, bob.getId(), "Fred"));
+
+        final Throwable thrown = catchThrowable(() -> overrideRepository
+                .save(new ExampleOverrideWrite(null, bob.getId(), "Howard")));
+        final Optional<Throwable> expected = Stream
+                .iterate(thrown, Objects::nonNull, Throwable::getCause)
+                .filter(t -> t instanceof DuplicateKeyException)
+                .findFirst();
+        assertThat(expected).isNotEmpty();
     }
 }
